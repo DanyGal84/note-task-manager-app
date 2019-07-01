1e854eff90a0e292deda3d7de234aed0fd5c01e7
Daniel Galvan <danygs@gmail.com>
2019-07-01 13:43:50 -0500

    Sort and pagination added to the querys

--- src/index.js
+++ src/index.js
@@ -18,18 +18,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log(`Server is up in port ${port}`)
 })
-
-// const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-  // const task = await Task.findById('5d1a2e86c55ba73128e5e7e8')
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner)
-
-  const user = await User.findById('5d1a2da7bb28eb3090db065c')
-  await user.populate('tasks').execPopulate()
-  console.log(user.tasks)
-}
-
-main()
